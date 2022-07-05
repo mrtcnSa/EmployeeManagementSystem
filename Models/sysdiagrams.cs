0e71b2212b8832a01ba45a1bca6329ad626a6c75
mrtcnSa <108678871+mrtcnSa@users.noreply.github.com>
2022-07-05 19:36:41 +0300

    Add files via upload

--- Models/sysdiagrams.cs
+++ Models/sysdiagrams.cs
@@ -12,12 +12,12 @@ namespace EmployeeManagementSystem.Models
     using System;
     using System.Collections.Generic;
     
-    public partial class TblProject
+    public partial class sysdiagrams
     {
-        public int IDProject { get; set; }
-        public string ProjectName { get; set; }
-        public Nullable<int> IDEmployee { get; set; }
-    
-        public virtual TblEmployee TblEmployee { get; set; }
+        public string name { get; set; }
+        public int principal_id { get; set; }
+        public int diagram_id { get; set; }
+        public Nullable<int> version { get; set; }
+        public byte[] definition { get; set; }
     }
 }
