3acb40cadc3f0721b777d961b431fce5c19d856f
Mertcan <Mertcan@LAPTOP-PST4TSKL>
2022-07-05 18:22:19 +0300

    First commit

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,90 @@
+﻿using EmployeeManagementSystem.Concrete;
+using EmployeeManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeManagementSystem.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        EmployeeEntities db = new EmployeeEntities();
+        GenericRepository<TblEmployee> gr = new GenericRepository<TblEmployee>();
+        GenericRepository<TblJobPosition> job = new GenericRepository<TblJobPosition>();
+        GenericRepository<TblDepartment> dep = new GenericRepository<TblDepartment>();
+        public ActionResult Index()
+        {
+            var values = gr.List();
+            return View(values);
+        }
+        [HttpGet]
+        public ActionResult AddEmployee()
+        {
+
+            List<SelectListItem> valueDep = (from x in dep.List()
+                                                select new SelectListItem
+                                                {
+                                                    Text = x.DepartmentName,
+                                                    Value = x.IDDepartment.ToString()
+                                                }).ToList();
+
+             
+            ViewBag.valueDep = valueDep;
+
+            List<SelectListItem> valueJob = (from x in job.List()
+                                                  select new SelectListItem
+                                                  {
+                                                      Text = x.JobPosition,
+                                                      Value = x.IDJobPosition.ToString()
+                                                  }).ToList();
+            ViewBag.valueJob = valueJob;
+            
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddEmployee(TblEmployee e)
+        {
+
+            gr.Insert(e);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult EditEmployee(int id)
+        {
+            List<SelectListItem> valueDep = (from x in dep.List()
+                                             select new SelectListItem
+                                             {
+                                                 Text = x.DepartmentName,
+                                                 Value = x.IDDepartment.ToString()
+                                             }).ToList();
+
+
+            ViewBag.valueDep = valueDep;
+
+            List<SelectListItem> valueJob = (from x in job.List()
+                                             select new SelectListItem
+                                             {
+                                                 Text = x.JobPosition,
+                                                 Value = x.IDJobPosition.ToString()
+                                             }).ToList();
+            ViewBag.valueJob = valueJob;
+            var value = gr.Get(x => x.IDEmployee == id);
+            return View(value);
+        }
+        [HttpPost]
+        public ActionResult EditEmployee(TblEmployee e)
+        {
+            gr.Update(e);
+            return RedirectToAction("Index");
+        }
+        public ActionResult DeleteEmployee(int id)
+        {
+            var value = gr.Get(x => x.IDEmployee == id); 
+            
+            gr.Delete(value);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Controllers/ProjectsController.cs
@@ -0,0 +1,77 @@
+﻿using EmployeeManagementSystem.Concrete;
+using EmployeeManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeManagementSystem.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        EmployeeEntities db = new EmployeeEntities();
+        GenericRepository<TblEmployee> gr = new GenericRepository<TblEmployee>();
+        GenericRepository<TblJobPosition> job = new GenericRepository<TblJobPosition>();
+        GenericRepository<TblDepartment> dep = new GenericRepository<TblDepartment>();
+        GenericRepository<TblProject> pro = new GenericRepository<TblProject>();
+        public ActionResult Index()
+        {
+            var values = pro.List();
+            return View(values);
+            
+        }
+        [HttpGet]
+        public ActionResult AddProjectAssignEmployee()
+        {
+            List<SelectListItem> valueEmp = (from x in gr.List()
+                                             select new SelectListItem
+                                             {
+                                                 Text = x.FirstName + " " + x.MiddleName + " " + x.LastName,
+                                                 Value = x.IDEmployee.ToString()
+                                             }).ToList();
+
+
+            ViewBag.valueEmp = valueEmp;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddProjectAssignEmployee(TblProject p)
+        {
+
+            pro.Insert(p);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult EditProject(int id)
+        {
+            List<SelectListItem> valueEmp = (from x in gr.List()
+                                             select new SelectListItem
+                                             {
+                                                 Text = x.FirstName+" " + x.MiddleName + " " + x.LastName,
+                                                 Value = x.IDEmployee.ToString()
+                                             }).ToList();
+
+
+            ViewBag.valueEmp = valueEmp;
+
+           
+            var value = pro.Get(x => x.IDProject == id);
+            return View(value);
+        }
+        [HttpPost]
+        public ActionResult EditProject(TblProject p)
+        {
+            pro.Update(p);
+            return RedirectToAction("Index");
+        }
+        public ActionResult DeleteProject(int id)
+        {
+            var value = pro.Get(x => x.IDProject == id);
+
+            pro.Delete(value);
+            return RedirectToAction("Index");
+        }
+    }
+}
+ 
